14ac3bd0009811ac66f6af4254e3cdaa1ae967ad
rirepeirs <rirepeirs@gmail.com>
2025-03-02 21:57:01 -0500

    Added contact.html and updated header.html

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Email
 
 class MyForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
     subject = StringField("Subject", validators=[DataRequired()])
     message = TextAreaField("Message", validators=[DataRequired()])
     send = SubmitField("Send")
