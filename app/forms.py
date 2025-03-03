37d92d0fb047755e05a8bfb089e2166e2abfde84
rirepeirs <rirepeirs@gmail.com>
2025-03-02 20:35:29 -0500

    Added to config and init and created env and forms.py

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField 
+from wtforms.validators import DataRequired, Email
+
+class MyForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    subject = StringField("Subject", validators=[DataRequired()])
+    message = TextAreaField("Message", validators=[DataRequired()])
+    send = SubmitField("Send")
